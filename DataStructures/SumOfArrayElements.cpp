1e69669b9643b6d0ba12c7e34ef58df1c42e811b
kishore2106 <kishore210601krish@gmail.com>
2024-02-22 06:08:44 +0530

    Sum Of Arrays

---
+++ DataStructures/SumOfArrayElements.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumOfArray(int arr[], int n){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int sum = sumOfArray(arr,n);
+
+    cout<<sum<<endl;
+
+    return 0;
+}
