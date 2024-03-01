0ce31e3f0941c97ada9c1ff3c81d7a3e80606268
kishore2106 <kishore210601krish@gmail.com>
2024-03-01 20:08:17 +0530

    cin.getline()

---
+++ DataStructures/DS Character Array/cin.getline().cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char name[7];
+    //cin.getline(<input_string>,<length_of_the_string>,<delimiter_char>)
+    cin.getline(name, 6,'i'); 
+    cout<<name;
+
+    return 0;
+}
